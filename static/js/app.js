ca3407eeac4fdbc942d93cb634b9ff709d21bf38
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 <galluccimia@gmail.com>
2022-12-21 23:35:41 -0500

    function init and bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -5,18 +5,28 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
-
 let sampleData;
 let metaData;
 
+function init () {
+let values = object.values(samples.sample_values)
+let labels = object.values(samples.otu_ids)
+let hovertext = object.values(samples.otu_labels)
+
 let trace = {
-      x: ['name', 'name', 'name'],
-      y: [1, 2, 3],
-      type: 'bar'
+      x: [0, 50, 100, 150],
+      y: [sample_values],
+      type: 'bar',
+      text: [hovertext]
     };
   
-  Plotly.newPlot('myDiv', data);
+  Plotly.newPlot('container', trace);
+
+  // Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
+}
+
+// init function
+init();
